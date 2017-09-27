51d66f01833a22b946a0764c136de71c02abd005
erick.kirabui <erick.kirabui@strathmore.edu>
2017-09-27 11:02:05 -0400

    ability to set location with searching and clicking map

--- location.js
+++ location.js
@@ -1,6 +1,37 @@
-const API_KEY = 'AIzaSyBixSBMVXJ-a_8a7VNzXttqmCIm7GP1WbU';
+var API_KEY = 'AIzaSyBixSBMVXJ-a_8a7VNzXttqmCIm7GP1WbU';
+
 //MAIN MAP
-let MAP;
+var MAP;
+var MARKER;
+
+//SET LOCATION ON THE INPUT ELEMENTS
+function setLocation(LatLng,pan){
+	addMarker({coords:LatLng});
+	document.getElementById('long').value = LatLng.lng;
+	document.getElementById('lat').value = LatLng.lat;
+	if(pan){
+		MAP.setCenter(LatLng);
+    MAP.setZoom(15);
+	}
+}
+
+//GET LATLNG THROUGH GEOCODING
+function getLatLng(){
+  var address = document.getElementById('addressInput').value;
+  geocode(address)
+  .then(function(response){
+		var l = response.data.results[0].geometry.location;
+    setLocation(response.data.results[0].geometry.location,true);
+  })
+  .catch(function(error){
+    console.log(error)
+  });
+}
+
+//Show the list of locations
+function showResults(results){
+	
+}
 
 //GEO LOCATING
 function geolocate(props){
@@ -16,7 +47,7 @@ function geolocate(props){
             res(response);
           })
           .catch(function(error){
-            rej(error);
+            console.log(error);
           });
       });
     }
@@ -25,47 +56,50 @@ function geolocate(props){
 
 //GEO-CODING
 function geocode(location){
-  axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+ return axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
     params:{
       address: location,
       key: API_KEY
     }
   })
-  .then(function(response){
-    console.log(response);
-  })
-  .catch(function(error){
-    console.log(error)
-  })
 }
 
 //CREATING MAP
 function initMap(){
-  geolocate().then(function(location){
-    MAP = new google.maps.Map(document.getElementById('map'),{
-      zoom: 8,
-      center: {lat:location.latitude,lng:location.longitude}
-    });
-  });
-  google.maps.event.addListener(map,'click',function(event){
-      addMarker({coords:event.latLng})
-    });
+  geolocate()
+		.then(function(location){
+			MAP = new google.maps.Map(document.getElementById('map'),{
+				zoom: 8,
+				center: {lat:location.latitude,lng:location.longitude}
+			});
+		
+			MAP.addListener('click',function(event){
+				setLocation({lat: event.latLng.lat(), lng: event.latLng.lng()},false);
+  		});
+		});  
 }
 
 //ADDING MARKER
 function addMarker(props){
-  var marker = new google.maps.Marker({
-    position: props.coords,
-    map:MAP,
-  });
+	//if marker exists set new position
+	if(MARKER){
+		MARKER.setPosition(props.coords);
+	}else{
+		MARKER = new google.maps.Marker({
+			position: props.coords,
+			map:MAP,
+ 		});	
+	}
+  //icon
   if(props.icon){
-    marker.setIcon(props.icon);
+    MARKER.setIcon(props.icon);
   }
+	//content
   if(props.content){
     var infoWindow = new google.maps.InfoWindow({
       content: props.content
     });
-    marker.addListener('click',function(){
+    MARKER.addListener('click',function(){
       infoWindow.open(map,marker);
     })
   }
